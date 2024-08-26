c25ca883d598eeec2f03d069cc424d01e26fc851
Gabriel Braga <gabrielferreira9034@gmail.com>
2024-08-26 18:55:28 -0300

    Adicionando todos os arquivos do projeto

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['localhost'], // Adicione outros domínios conforme necessário
-  },
-  webpack(config) {
-    // Adicione suas configurações personalizadas do Webpack aqui
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
